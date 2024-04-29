e1528edc281e2f30637732fdaa58432b957ee01c
ひろ <koki.hiro0723@gmail.com>
2024-04-29 18:57:09 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,42 +1,47 @@
 import Head from "next/head";
 import { Main } from "@/src/components/Main"; // Changed import from "next/document" to "@/components/Main"
 import { Header } from "@/src/components/Header";
-import { useCallback, useEffect } from "react";
+import styles from "@/src/styles/Home.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1);
 
   // onClickの処理
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault(); // デフォルトのイベントをキャンセルする
-    alert(foo);
-  }, []);
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
+  // const handleClick = useCallback((e) => {
+  //   console.log(e.target.href);
+  //   e.preventDefault(); // デフォルトのイベントをキャンセルする
+  //   alert(foo);
+  // }, []);
 
   // ページが読み込まれた時のイベント
   useEffect(() => {
-    console.log("マウント時");
+    // console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
 
     return () => {
-      console.log("アンマウント時");
+      // console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
   }, []);
 
   return (
-    <>
+    <div className={styles.main}>
       <Head>
         <title>Index Page</title>
       </Head>
 
       <Header />
 
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
+      <h1>{count}</h1>
+
+      <button onClick={handleClick}>ボタン</button>
 
       <Main page="index" />
-    </>
+    </div>
   );
 }
