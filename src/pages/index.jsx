575331d0e59de3ac46aba8480f95c7f1e3c804a8
ひろ <koki.hiro0723@gmail.com>
2024-04-29 18:03:34 +0900

    ✨ 説明用のuseEffectの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,27 @@
 import Head from "next/head";
 import { Main } from "@/src/components/Main"; // Changed import from "next/document" to "@/components/Main"
 import { Header } from "@/src/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
   const foo = 1;
 
+  // onClickの処理
   const handleClick = useCallback((e) => {
     console.log(e.target.href);
-    e.preventDefault();
+    e.preventDefault(); // デフォルトのイベントをキャンセルする
     alert(foo);
+  }, []);
+
+  // ページが読み込まれた時のイベント
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
