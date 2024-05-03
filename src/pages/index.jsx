17fb72e1f7ca9ea94a2e1cd5ce8e25b1fdceb726
ひろ <koki.hiro0723@gmail.com>
2024-05-04 01:50:47 +0900

    ⚡️ useCallback,useEffectの第2引数を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,32 +2,25 @@ import Head from "next/head";
 import { Main } from "@/src/components/Main"; // Changed import from "next/document" to "@/components/Main"
 import { Header } from "@/src/components/Header";
 import styles from "@/src/styles/Home.module.css";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  // onClickの処理
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
-  // const handleClick = useCallback((e) => {
-  //   console.log(e.target.href);
-  //   e.preventDefault(); // デフォルトのイベントをキャンセルする
-  //   alert(foo);
-  // }, []);
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   // ページが読み込まれた時のイベント
   useEffect(() => {
-    // console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
 
     return () => {
-      // console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [count]);
 
   return (
     <div className={styles.main}>
