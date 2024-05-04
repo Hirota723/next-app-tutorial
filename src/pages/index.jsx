d9473f2ed9cf7fad38947aeeebfe2c5946dc4462
ひろ <koki.hiro0723@gmail.com>
2024-05-04 17:15:06 +0900

    ✨ useStateで文字列と真偽値について解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,12 +6,26 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
       setCount((count) => count + 1);
     }
   }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
 
   // ページが読み込まれた時のイベント
   useEffect(() => {
@@ -30,9 +44,11 @@ export default function Home() {
 
       <Header />
 
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
 
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
 
       <Main page="index" />
     </div>
